5c430a56d0e05581569f8af7dc34281b60f0410e
Gabriel Falcão <99846326+Gabrielhfalcao@users.noreply.github.com>
2023-02-09 16:48:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function mostrarOpcaoMenu(){
     var btnRelatorio1 = document.getElementById('btnRelatorio1')
     var btnRelatorio= document.getElementById('btnRelatorio')
     var opcaoMenu1 = document.getElementById('opcaoMenu1')
-    var arrowBtn = document.getElementsByClassName('glyphicon glyphicon-triangle-bottom')
     opcaoMenu1.style.height = '160px'
     opcaoMenu1.style.transition = '.5s'
     btnRelatorio1.style.display = 'none'
