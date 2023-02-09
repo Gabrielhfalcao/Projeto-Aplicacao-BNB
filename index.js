6306aa6fe5937606706373ee1f14a01e4be7f49b
Gabriel Falcão <99846326+Gabrielhfalcao@users.noreply.github.com>
2023-02-09 11:02:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function mostrarOpcaoMenu(){
     var btnRelatorio1 = document.getElementById('btnRelatorio1')
     var btnRelatorio= document.getElementById('btnRelatorio')
     var opcaoMenu1 = document.getElementById('opcaoMenu1')
+    var arrowBtn = document.getElementsByClassName('glyphicon glyphicon-triangle-bottom')
     opcaoMenu1.style.height = '160px'
     opcaoMenu1.style.transition = '.5s'
     btnRelatorio1.style.display = 'none'
@@ -16,5 +17,22 @@ function voltarOpcaoMenu(){
     opcaoMenu1.style.transition = '.5s'
     btnRelatorio1.style.display = 'block'
     btnRelatorio.style.display = 'none'
-    
+}
+
+function desfocarFundo(){
+    var menuLateral = document.getElementById('menuLateral')
+    var tituloPortal = document.getElementById('tituloPort')
+    menuLateral.style.filter = 'blur(4px)'
+    menuLateral.style.transition = '.5s'
+    tituloPortal.style.filter = 'blur(4px)'
+    tituloPortal.style.transition = '.5s'
+}
+
+function focarFundo(){
+    var menuLateral = document.getElementById('menuLateral')
+    var tituloPortal = document.getElementById('tituloPort')
+    menuLateral.style.filter = 'blur(0px)'
+    menuLateral.style.transition = '.5s'
+    tituloPortal.style.filter = 'blur(0px)'
+    tituloPortal.style.transition = '.5s'
 }
